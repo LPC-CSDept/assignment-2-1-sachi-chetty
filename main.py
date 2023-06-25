a1137a72aab55990328d4a1fb02bc4df81ac3f35
Sachi Chetty <sachi.chetty@gmail.com>
2023-06-24 21:53:22 -0700

    Committing Assignment 2-1

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-    male = 60
-    female = 40
+    male = float(input("males"))
+    female = float(input("females"))
         
     total = male + female
 
-    m_perc = male / total * 100
-    f_perc = female / total * 100
+    m_perc = (male / total) * 100.0
+    f_perc = (female / total) * 100.0
 
-    print (f"Total number of students: {total}")
-    print (f"The numbers of males and females: {male} {female}")
-    print (f"The percentages of males and females: {m_perc}% {f_perc}%")
+    print (f"Total number of students: {int(total)}")
+    print (f"The numbers of males and females: {int(male)} {int(female)}")
+    print (f"The percentages of males and females: {m_perc:.2f}% {f_perc:.2f}%")
     """
     ########################################
     # Do not delete the return statement
